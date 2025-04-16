422f0331e6657c10d499827dbdd058f4fbf57c15
chanwook <cksdnr1@gmail.com>
2025-04-17 04:11:45 +0900

    [green] output file exist check TC

--- SSD/src/SsdInterface.cpp
+++ SSD/src/SsdInterface.cpp
@@ -4,6 +4,13 @@ bool SsdInterface::Write(std::string lba, std::string dataPattern) {
   return true;
 }
 
-void SsdInterface::Read(std::string lba) {}
+void SsdInterface::Read(std::string lba) { checkOutputFileExist(); }
 
 std::string SsdInterface::GetResult() { return "nothing"; }
+
+void SsdInterface::checkOutputFileExist() {
+  std::ofstream file(outputFileName, std::ios::app);  // 없으면 생성
+  if (file.is_open()) {
+    file << "content" << "\n";
+  }
+}
