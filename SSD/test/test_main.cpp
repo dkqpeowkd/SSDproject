1c037fb1ca109848abeca766cc0e16cb27b178dd
박민수 <pms5917@gmail.com>
2025-04-17 08:33:50 +0900

    [RED] 요구사항 명세서 기반으로 UnitTest 작성

--- SSD/test/test_main.cpp
+++ SSD/test/test_main.cpp
@@ -1,7 +1,91 @@
 #include "gmock/gmock.h"
 #include <iostream>
+#include <string>
 
-TEST(TS, TC) { EXPECT_EQ(1, 1); }
+// - 콘솔 출력 없음
+TEST(SSD, Write_Pass) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string lba = "0";
+  std::string dataPattern = "0x12345678";
+  ssdInterface->Write(lba, dataPattern);
+
+  EXPECT_EQ("nothing", ssdInterface->GetResult());
+}
+
+// - ssd_nand.txt 파일에 해당 LBA 위치에 값 저장
+// - ssd_nand.txt에서 해당 LBA 값 읽어 ssd_output.txt에 기록
+TEST(SSD, Read_Mapped) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string lba = "0";
+  std::string dataPattern = "0x12345678";
+  ssdInterface->Write(lba, dataPattern);
+  ssdInterface->Read(lba);
+
+  EXPECT_EQ(dataPattern, ssdInterface->GetResult());
+}
+
+// - 미기록 LBA는 0x00000000 기록
+TEST(SSD, Read_Unmapped) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string ZERO_PATTERN = "0x00000000";
+
+  EXPECT_EQ(ZERO_PATTERN, ssdInterface->GetResult());
+}
+
+// - 콘솔 출력 없음
+TEST(SSD, Write_Pass_After_Read) {
+  SsdInterface* ssdInterface = new SsdInterface();
+
+  std::string ZERO_PATTERN = "0x00000000";
+
+  EXPECT_EQ(ZERO_PATTERN, ssdInterface->GetResult());
+
+  std::string lba = "0";
+  std::string dataPattern = "0x12345678";
+  ssdInterface->Write(lba, dataPattern);
+
+  EXPECT_EQ("nothing", ssdInterface->GetResult());
+}
+
+// - LBA 범위: 0~99, 10진수
+// - 잘못된 LBA/Value 입력 시 ssd_output.txt에 "ERROR" 기록
+TEST(SSD, Write_Fail_OutOfRange) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string lba = "100";
+  std::string dataPattern = "0x12345678";
+  ssdInterface->Write(lba, dataPattern);
+
+  EXPECT_EQ("ERROR", ssdInterface->GetResult());
+}
+
+// - Value: 0x로 시작하는 8자리 16진수(총 10글자, 예: 0x1234ABCD)
+// - 잘못된 LBA/Value 입력 시 ssd_output.txt에 "ERROR" 기록
+TEST(SSD, Write_Fail_InvalidPattern_0) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string lba = "0";
+  std::string dataPattern = "0x1";
+  ssdInterface->Write(lba, dataPattern);
+
+  EXPECT_EQ("ERROR", ssdInterface->GetResult());
+}
+
+TEST(SSD, Write_Fail_InvalidPattern_1) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string lba = "0";
+  std::string dataPattern = "1234567890";
+  ssdInterface->Write(lba, dataPattern);
+
+  EXPECT_EQ("ERROR", ssdInterface->GetResult());
+}
+
+// - 잘못된 LBA 입력 시 ssd_output.txt에 "ERROR" 기록
+TEST(SSD, Read_Fail_OutOfRange) {
+  SsdInterface* ssdInterface = new SsdInterface();
+  std::string lba = "100";
+  ssdInterface->Read(lba);
+
+  EXPECT_EQ("ERROR", ssdInterface->GetResult());
+}
 
 int main() { 
 	testing::InitGoogleMock();
