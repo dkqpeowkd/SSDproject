98e0518be83a573adacde3cdb92ee8f72068f49e
chanwook <cksdnr1@gmail.com>
2025-04-17 05:49:49 +0900

    main.cpp edited for cmd

--- SSD/src/SsdInterface.h
+++ SSD/src/SsdInterface.h
@@ -6,6 +6,7 @@ class SsdInterface {
  public:
   void Write(std::string lba, std::string dataPattern);
   void Read(std::string lba);
+  bool isValidNumberZeroToNintyNine(const std::string& str);
 
   std::string GetResult();
 
@@ -22,7 +23,7 @@ class SsdInterface {
 
   bool checkNandFileExist();
 
-  bool isValidNumberZeroToNintyNine(const std::string& str);
+
   bool isValidDataPattern(std::string dataPattern);
 
   unsigned int dataPatterToDigit(std::string dataPattern);
--- SSD/src/main.cpp
+++ SSD/src/main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <string>
+#include <regex>
 
-int main() {
-  std::cout << "this is source main" << std::endl;
+#include "SsdInterface.h"
+
+int main(int argc, char* argv[]) {
+  SsdInterface ssdInterface;
+  try {
+    // 명령어 개수 검사
+    if (argc < 3) {
+      throw std::invalid_argument("Insufficient arguments.");
+    }
+
+    std::string command = argv[1];
+    std::string lbaStr = argv[2];
+
+    if (command == "R") {
+      if (argc != 3) {
+        throw std::invalid_argument("Insufficient read arguments.");
+      }
+
+      ssdInterface.Read(lbaStr);
+    } else if (command == "W") {
+      if (argc != 4) {
+        throw std::invalid_argument("Insufficient write arguments.");
+      }
+
+      std::string value = argv[3];
+
+      ssdInterface.Write(lbaStr, value);
+    } else {
+      throw std::invalid_argument("Insufficient comment.");
+    }
+  } catch (const std::exception& e) {
+    const std::string errorString = "ERROR";
+    ssdInterface.Read(errorString);
+    return 1;
+  }
+
   return 0;
 }
