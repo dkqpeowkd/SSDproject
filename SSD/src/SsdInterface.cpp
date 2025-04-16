647abde6851f4ca061ee6b6429b6d3ce21b5e44d
chanwook <cksdnr1@gmail.com>
2025-04-17 04:24:01 +0900

    [green] Read_Unmapped TC

--- SSD/src/SsdInterface.cpp
+++ SSD/src/SsdInterface.cpp
@@ -4,13 +4,15 @@ bool SsdInterface::Write(std::string lba, std::string dataPattern) {
   return true;
 }
 
-void SsdInterface::Read(std::string lba) { checkOutputFileExist(); }
+void SsdInterface::Read(std::string lba) { recordZeroPatternToOutputFile(); }
 
-std::string SsdInterface::GetResult() { return "nothing"; }
+std::string SsdInterface::GetResult() { return output; }
 
-void SsdInterface::checkOutputFileExist() {
-  std::ofstream file(outputFileName, std::ios::app);  // 없으면 생성
+void SsdInterface::recordZeroPatternToOutputFile() {
+  output = ZERO_PATTERN;
+
+  std::ofstream file(OUTPUT_FILE_NAME, std::ios::trunc); 
   if (file.is_open()) {
-    file << "content" << "\n";
+    file << output << "\n";
   }
 }
