40fdceb6804f7a3453a064ccfd0ad33dd012b4e3
RauenDaddy <hkkim2709@gmail.com>
2025-04-17 10:33:30 +0900

    [GREEN] Write Pass TC pass

--- SSD/src/SsdInterface.cpp
+++ SSD/src/SsdInterface.cpp
@@ -1,7 +1,27 @@
 #include "SsdInterface.h"
 
-bool SsdInterface::Write(std::string lba, std::string dataPattern) {
-  return true;
+#include <cctype>
+#include <sstream>
+
+void SsdInterface::Write(std::string lba, std::string dataPattern) {
+  if (isValidNumberZeroToNintyNine(lba) == false || isValidDataPattern(dataPattern) == false) {
+    recordErrorPatternToOutputFile();
+    return;
+  }
+
+  int offset = std::stoi(lba) * 4;
+  unsigned int numDataPatter = dataPatterToDigit(dataPattern);
+
+  if (checkNandFileExist() == false) {
+    std::ofstream outfile(NAND_FILE_NAME, std::ios::binary);
+  }
+
+  std::fstream nandFile(NAND_FILE_NAME, std::ios::binary | std::ios::in | std::ios::out);
+  nandFile.seekp(offset, std::ios::beg);
+  nandFile.write(reinterpret_cast<const char*>(&numDataPatter),
+             sizeof(numDataPatter));
+
+
 }
 
 void SsdInterface::Read(std::string lba) { 
@@ -33,16 +53,39 @@ void SsdInterface::recordErrorPatternToOutputFile() {
 }
 
 bool SsdInterface::isValidNumberZeroToNintyNine(const std::string& str) {
-  // 모든 문자가 숫자인지 확인
   for (char ch : str) {
     if (!std::isdigit(ch)) return false;
   }
   int num = std::stoi(str);
-  // 숫자 범위가 0~99인지 확인
+
   try {
     int num = std::stoi(str);
     return num >= 0 && num <= 99;
   } catch (...) {
     return false;
   }
+}
+
+bool SsdInterface::checkNandFileExist() {
+  std::ifstream nandFile(NAND_FILE_NAME);
+  return nandFile.is_open();
+}
+  
+bool SsdInterface::isValidDataPattern(std::string dataPattern) {
+  if (dataPattern.length() != 10) return false;
+
+  if (dataPattern[0] != '0' || dataPattern[1] != 'x') return false;
+
+  return true;
+}
+
+unsigned int SsdInterface::dataPatterToDigit(std::string dataPattern) {
+  std::string dataPatterWithout0x = dataPattern.substr(2);
+
+  unsigned int numUint;
+  std::stringstream ss;
+  ss << std::hex << dataPatterWithout0x;
+  ss >> numUint;
+  
+  return numUint;
 }
