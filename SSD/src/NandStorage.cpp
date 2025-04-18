6602aa238a4ad8ec056515cf2bf5c67f9a3ea83b
RauenDaddy <hkkim2709@gmail.com>
2025-04-18 13:38:14 +0900

    [REFACTOR] File에 대한 Exception handling 추가
    file 생성/접근/수정 등이 안되면 error return 되도록 수정

--- SSD/src/NandStorage.cpp
+++ SSD/src/NandStorage.cpp
@@ -8,11 +8,15 @@
 #include <iomanip>
 #include "NandStorage.h"
 
-void NandStorage::Write(const std::string& lba, const std::string& dataPattern) {
+bool NandStorage::Write(const std::string& lba, const std::string& dataPattern) {
   if (IsExistNand() == false) {
-    std::ofstream outfile(NAND_FILE_NAME, std::ios::binary);
+    if (createNandFile() == false) {
+      return false;
+    }
 
-    nandFileInitialize();
+    if (initializeNandFile() == false) {
+      return false;
+    }
   }
 
   int offset = std::stoi(lba) * LBA_SIZE;
@@ -23,16 +27,31 @@ void NandStorage::Write(const std::string& lba, const std::string& dataPattern) 
   nandFile.seekp(offset, std::ios::beg);
   nandFile.write(reinterpret_cast<const char*>(&numDataPattern),
                  sizeof(numDataPattern));
+
+  return true;
 }
 
-void NandStorage::nandFileInitialize() {
+bool NandStorage::createNandFile() {
+  std::ofstream outfile(NAND_FILE_NAME, std::ios::binary);
+  return outfile.is_open();
+}
+
+bool NandStorage::initializeNandFile() {
   std::fstream nandFile(NAND_FILE_NAME,
                         std::ios::binary | std::ios::in | std::ios::out);
   const int nandOffset = MAX_LBA * LBA_SIZE;
   nandFile.seekp(nandOffset, std::ios::beg);
+  if (nandFile.fail() == true) {
+    return false;
+  }
 
   constexpr int ZERO = 0;
   nandFile.write(reinterpret_cast<const char*>(&ZERO), sizeof(ZERO));
+  if (nandFile.fail()) {
+    return false;
+  }
+
+  return true;
 }
 
 bool NandStorage::IsExistNand() {
@@ -42,12 +61,18 @@ bool NandStorage::IsExistNand() {
 
 unsigned int NandStorage::Read(const std::string& lba) {
   std::ifstream file(NAND_FILE_NAME, std::ios::binary);
+  if (!file.is_open()) {
+    return 0;
+  }
 
   int offset = std::stoi(lba) * LBA_SIZE;
   file.seekg(offset, std::ios::beg);
 
   unsigned int value = 0;
   file.read(reinterpret_cast<char*>(&value), sizeof(value));
+  if (file.fail()) {
+    return 0;
+  }
 
   return value;
 }
