1e94d0250e7883d49178bdc6db8e4aa27ec96e0b
RauenDaddy <hkkim2709@gmail.com>
2025-04-18 16:31:26 +0900

    [RED] 음수 LBA, SIZE에 대한 Erase 처리 추가

--- SSD/src/SsdInterface.cpp
+++ SSD/src/SsdInterface.cpp
@@ -47,12 +47,25 @@ void SsdInterface::Read(std::string lba) {
 }
 
 void SsdInterface::Erase(std::string lba, std::string scope) {
+  if (validator.IsNumberWithinRange(scope, 1, 10, true) == false) {
+    return recoder.RecordErrorPatternToOutputFile(validator.GetErrorReason());
+  }
+
   if (validator.IsNumberWithinRange(lba, 0, MAX_LBA) == false) {
     return recoder.RecordErrorPatternToOutputFile(validator.GetErrorReason());
   }
 
-  if (validator.IsNumberWithinRange(scope, 1, 10) == false) {
+  int numLba = stoi(lba);
+  int numScope = stoi(scope);
+  std::string endLba = std::to_string(numLba + numScope);
+
+  if (validator.IsNumberWithinRange(endLba, 0, MAX_LBA) == false) {
     return recoder.RecordErrorPatternToOutputFile(validator.GetErrorReason());
+  }
+
+  if (numScope < 0) {
+    lba = endLba;
+    scope = std::to_string(abs(numScope));
   }
 
   if (isBufferFull() == true) {
