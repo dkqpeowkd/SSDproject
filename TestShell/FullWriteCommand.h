ed2d1b1a2a4699f86937f9997faf918bd371964e
hyonisss <gyeun1212@gmail.com>
2025-04-18 14:29:52 +0900

    FullWrite 함수 객체화

---
+++ TestShell/FullWriteCommand.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ICommand.h"
+#include <string>
+#include <vector>
+
+class FullWriteCommand : public ICommand {
+public:
+    FullWriteCommand() = default;
+
+    const std::string& getCommandString() override;
+    const std::string& getUsage() override;
+    bool isValidArguments(const std::string& cmd, std::vector<std::string>& args) override;
+    bool Execute(const std::string& cmd, std::vector<std::string>& args) override;
+
+protected:
+    virtual int callSystem(const std::string& cmd);
+    virtual std::string readOutput();
+};
