66a28cd21b32cdcd1e5f9f15eef655a6393f3501
chanwook <cksdnr1@gmail.com>
2025-04-18 10:34:53 +0900

    [red] erase TC added

--- SSD/test/test_main.cpp
+++ SSD/test/test_main.cpp
@@ -13,7 +13,14 @@ constexpr const char* VALID_VALUE = "0x12345678";
 constexpr const char* INVALID_VALUE_1 = "0x1";
 constexpr const char* INVALID_VALUE_2 = "1234567890";
 
+constexpr const char* VALID_ERASE_SCOPE_1 = "1";
+constexpr const char* VALID_ERASE_SCOPE_6 = "6";
+constexpr const char* VALID_ERASE_SCOPE_10 = "10";
+constexpr const char* INVALID_ERASE_SCOPE_11 = "11";
+
+
 constexpr const char* VALID_LBA_BEGIN = "0";
+constexpr const char* VALID_LBA_95 = "95";
 constexpr const char* VALID_LBA_END = "99";
 constexpr const char* INVALID_LBA = "100";
 
@@ -29,6 +36,50 @@ class SSDTest : public ::testing::Test {
     if (fs::exists(NAND_FILE_NAME)) fs::remove(NAND_FILE_NAME);
   }
 };
+
+TEST_F(SSDTest, ErasePassLBA0To1) {
+  for (int i = 0; i < 100; i++) {
+    ssdInterface->Write(std::to_string(i), VALID_VALUE);
+  }
+  ssdInterface->Erase(VALID_LBA_BEGIN, VALID_ERASE_SCOPE_1);
+  ssdInterface->Read(VALID_LBA_BEGIN);
+  EXPECT_EQ(ZERO_PATTERN, ssdInterface->GetResult());
+}
+
+TEST_F(SSDTest, ErasePassLBA0To10) {
+  for (int i = 0; i < 100; i++) {
+    ssdInterface->Write(std::to_string(i), VALID_VALUE);
+  }
+  ssdInterface->Erase(VALID_LBA_BEGIN, VALID_ERASE_SCOPE_10);
+  for (int i = 0; i < 10; i++) {
+    ssdInterface->Read(std::to_string(i));
+    EXPECT_EQ(ZERO_PATTERN, ssdInterface->GetResult());
+  }
+}
+
+TEST_F(SSDTest, ErasePassLBA0To11) {
+  for (int i = 0; i < 100; i++) {
+    ssdInterface->Write(std::to_string(i), VALID_VALUE);
+  }
+  ssdInterface->Erase(VALID_LBA_BEGIN, INVALID_ERASE_SCOPE_11);
+  EXPECT_NE(ERROR_PATTERN, ssdInterface->GetResult());
+}
+
+TEST_F(SSDTest, EraseErrorLBA95To6) {
+  for (int i = 0; i < 100; i++) {
+    ssdInterface->Write(std::to_string(i), VALID_VALUE);
+  }
+  ssdInterface->Erase(VALID_LBA_95, VALID_ERASE_SCOPE_6);
+  EXPECT_NE(ERROR_PATTERN, ssdInterface->GetResult());
+}
+
+TEST_F(SSDTest, EraseErrorInvalidLBA) {
+  for (int i = 0; i < 100; i++) {
+    ssdInterface->Write(std::to_string(i), VALID_VALUE);
+  }
+  ssdInterface->Erase(INVALID_LBA, VALID_ERASE_SCOPE_10);
+  EXPECT_NE(ERROR_PATTERN, ssdInterface->GetResult());
+}
 
 TEST_F(SSDTest, Write_Pass_0) {
   ssdInterface->Write(VALID_LBA_BEGIN, VALID_VALUE);
